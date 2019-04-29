69137328ab384201c9444eec4cafca887afc2aa1
CarolineCF <caroline.cessin-ferrand@le-campus-numerique.fr>
2019-04-29 11:34:07 +0200

    add SuppList & SuppMap

---
+++ new_appli/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SuppliersList msg="Liste des fournisseurs"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import SuppliersList from './components/SuppliersList.vue'
+import SuppliersMap from './components/SuppliersMap.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    SuppliersList,
+    
+  },
+  methods: {
+    onSuppliersListClick(){
+      window.alert("User click on");
+    },
+    onMapClick(){
+      window.alert("User click on");
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ new_appli/src/components/SuppliersMap.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Carte des fournisseurs</h1>
+</template>
+
+<script>
+export default {
+  name: 'SuppliersMap',
+  props: {
+    msg: String
+  }
+}
+</script>
